028bfa97953e9bc3045741bde80fc20174ec3dcf
bullyproof <44088685+bullyproof@users.noreply.github.com>
2018-10-12 13:05:29 +0200

    Anti.B Preliminary Site Upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:100,
+    autoplay:true,
+    autoplayHoverPause:true,
+    autoplayTimeout:5000,
+    margins:50,
+    animateIn: true,
+    items:4
+})
+});
